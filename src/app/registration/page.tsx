a08a61663c3dc2447857b14e70524146fa649817
harr1er23 <harr1er.work@gmail.com>
2023-10-23 14:46:45 +0300

    Realized Login & Registration layouts

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+"use client"
+import {useState} from 'react';
+import styles from "./styles.module.scss";
+import 'bootstrap/dist/css/bootstrap.css';
+import Input from "@/components/input";
+import { useRouter } from 'next/navigation';
 
 export default function Registration(){
+  const [nickName, setNickName] =useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [confirmPass, setConfirmPass] = useState('')
+  const router = useRouter();
   return (
-    <div>Registration</div>
+    <div className={styles.backgroundLogin}>
+      <h4>Регистрация</h4>
+      <div className="mb-3">
+        <div>Nick</div>
+        <Input
+          type={"text"}
+          value={nickName}
+          onChangeFunction={setNickName}
+          textPlaceholder={"Enter your nickname"}
+        />
+      </div>
+      <div className="mb-3">
+        <div>Email</div>
+        <Input
+          type={"email"}
+          value={email}
+          onChangeFunction={setEmail}
+          textPlaceholder={"Enter your email"}
+        />
+      </div>
+      <div className="mb-3">
+        <div>Password</div>
+        <Input
+          type={"password"}
+          value={pass}
+          onChangeFunction={setPass}
+          textPlaceholder={"Enter your password"}
+        />
+      </div>
+      <div className="mb-3">
+        <div>Confirm Password</div>
+        <Input
+          type={"password"}
+          value={confirmPass}
+          onChangeFunction={setConfirmPass}
+          textPlaceholder={"Confirm your password"}
+        />
+      </div>
+      <span onClick={() => router.replace("/login")} className={styles.buttonToLogin}>
+        Уже есть аккаунт?
+      </span>
+      <div className={styles.loginButtons}>
+        <div className="btn btn-primary">
+          Зарегистрироваться
+        </div>
+      </div>
+    </div>
   )
 }
